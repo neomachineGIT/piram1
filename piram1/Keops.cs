643e6c9c59cdf022f1364f09aaa1b4958b6ea782
neomachine <neomachinemail@gmail.com>
2023-07-01 11:14:46 -0500

    first commit

---
+++ piram1/Keops.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace piram1
+{
+    internal class Keops
+    {
+        public void Secuencia()
+        {
+            long decas = 1;
+            int esps = 18;
+            Console.SetCursorPosition(esps, 0);
+            Console.WriteLine("NEO -mach");
+            Console.SetCursorPosition(esps, 1);
+            Console.WriteLine("---------");
+            for (int i = 2; i <= 10; i++)
+            {
+                Console.SetCursorPosition(esps, i);
+                long operacion = decas * decas;
+                Console.WriteLine("{0} x {0} = {1}", decas, operacion);
+                decas = decas * 10 + 1;
+                esps -= 2;
+            }
+            Console.ReadKey();xzzggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggg        }
+
+    }
+}
---
+++ piram1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace piram1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Keops obj = new Keops();
+            obj.Secuencia();
+        }
+    }
+}
